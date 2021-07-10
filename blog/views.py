f6077f1095c264219de540f0352df8ec6cdf5242
ji-vvon <jjw9895@duksung.ac.kr>
2021-07-11 07:01:31 +0900

    7/11 과제

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import redirect, render, get_object_or_404
 from django.utils import timezone
+from django.core.paginator import Paginator
 from .models import Blog
+from .forms import BlogForm
 # Create your views here.
 
 def home(request):
     blogs = Blog.objects.all()
+    paginator = Paginator(blogs, 3)
+    page = request.GET.get('page')
+    blogs = paginator.get_page(page)
     return render(request, 'home.html', {'blogs' : blogs})
 
 def detail(request, id):
@@ -12,16 +17,17 @@ def detail(request, id):
     return render(request, 'detail.html', {'blog' : blog})
 
 def new(request):
-    return render(request, 'new.html')
+    form = BlogForm()
+    return render(request, 'new.html', {'form':form})
 
 def create(request):
-    new_blog = Blog()
-    new_blog.title = request.POST['title']
-    new_blog.writer = request.POST['writer']
-    new_blog.body = request.POST['body']
-    new_blog.pub_date = timezone.now()
-    new_blog.save()
-    return redirect('detail', new_blog.id)
+    form = BlogForm(request.POST, request.FILES)
+    if form.is_valid():
+        new_blog = form.save(commit=False)
+        new_blog.pub_date = timezone.now()
+        new_blog.save()
+        return redirect('detail', new_blog.id)
+    return redirect('home')
 
 def edit(request, id):
     edit_blog = Blog.objects.get(id = id)
